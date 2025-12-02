72a1c1165580d2015160435e7739dcc0a2bb950b
AlbertoLuna24 <jose.luna200804@gmail.com>
2025-12-02 12:30:36 -0600

    ruta nueva

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python control_escolar_desit_api/manage.py collectstatic --no-input
-python control_escolar_desit_api/manage.py migrate
+python control_escolar_desit_api/control_escolar_desit_api/manage.py collectstatic --no-input
+python control_escolar_desit_api/control_escolar_desit_api/manage.py migrate
