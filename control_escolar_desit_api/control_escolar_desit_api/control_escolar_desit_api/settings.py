bed532323c40cb78116d5bd4e2a17106e163ddc5
AlbertoLuna24 <jose.luna200804@gmail.com>
2025-12-03 23:00:26 -0600

    tengo sueño

--- control_escolar_desit_api/control_escolar_desit_api/control_escolar_desit_api/settings.py
+++ control_escolar_desit_api/control_escolar_desit_api/control_escolar_desit_api/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
 # Configuración de CORS: define orígenes permitidos y quita CORS_ORIGIN_ALLOW_ALL
 CORS_ALLOWED_ORIGINS = [
     #'http://localhost:4200',
-    "https://aplicacion-control-escolar-onmck5wfr.vercel.app",
+    "https://aplicacion-control-escolar-cyhysnr01.vercel.app",
     "https://aplicacion-control-escolar-4l0zoc24i.vercel.app",
     "https://aplicacion-control-escolar-l9nnug2iv.vercel.app", 
     'https://aplicacion-control-escolar.vercel.app',
